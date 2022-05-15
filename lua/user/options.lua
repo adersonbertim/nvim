18a2cd56dac7e06390362fb1d435173110a8ed99
Aderson Bertim <adersonbertim@gmail.com>
2022-05-14 23:08:43 -0300

    files and options configs

---
+++ lua/user/options.lua
@@ -0,0 +1,22 @@
+-- Basic Configs Options 
+vim.opt.smartcase = true
+vim.opt.smartindent = true
+vim.opt.splitbelow = true
+vim.opt.splitright = true
+vim.opt.ignorecase = true
+vim.opt.hlsearch = true
+vim.opt.pumheight = 10
+vim.opt.mouse = "a"
+-- vim.opt.termguicolors = true
+vim.opt.expandtab = true
+vim.opt.undofile = true
+vim.opt.shiftwidth = 2
+vim.opt.tabstop = 2
+vim.opt.number = true
+vim.opt.scrolloff = 8
+vim.opt.wrap = true
+vim.opt.guifont = "monospace:h17"
+vim.opt.fileencoding = "utf-8" 
+vim.cmd "set whichwrap+=<,>,[,],h,l"
+vim.cmd [[set iskeyword+=-]]
+vim.cmd [[set formatoptions-=cro]]

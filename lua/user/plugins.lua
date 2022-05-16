19a522b3b769a24167e491b41a7fcbf166a30779
Aderson Bertim <adersonbertim@gmail.com>
2022-05-16 17:29:25 -0300

    installing plugins in neovim

---
+++ lua/user/plugins.lua
@@ -0,0 +1,54 @@
+local fn = vim.fn
+
+-- Auto install packer
+local install_path = fn.stdpath "data" .. "/site/pack/packer/start/packer.nvim"
+if fn.empty(fn.glob(install_path)) > 0 then
+  PACKER_BOOTSTRAP = fn.system {
+    "git",
+    "clone",
+    "--depth",
+    "1",
+    "https://github.com/wbthomason/packer.nvim",
+    install_path,
+  }
+  print "Installing packer close and reopen Neovim..."
+  vim.cmd [[packadd packer.nvim]]
+end
+-- Autocommand, reload neovim with saving this file plugins.lua
+vim.cmd [[
+  augroup packer_user_config
+    autocmd!
+    autocmd BufWritePost plugins.lua source <afile> | PackerSync
+  augroup end
+]]
+
+-- Protected call
+local status_ok, packer = pcall(require, "packer")
+if not status_ok then
+  return
+end
+
+-- Packer use a popup window
+packer.init {
+  display = {
+    open_fn = function()
+      return require("packer.util").float {border = "rounded"}
+    end,
+  },
+}
+
+-- Install Plugins 
+return packer.startup(function(use)
+ --Plugins here
+  use "wbthomason/packer.nvim" -- Have packer manage itself
+  use "nvim-lua/plenary.nvim" -- Useful lua functions used ny lots of plugins
+  --Colorschemes
+  use "folke/tokyonight.nvim"
+
+
+
+
+  if PACKER_BOOTSTRAP then
+    require("packer").sync()
+  end
+end)

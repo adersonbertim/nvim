064c131fb079f990a9f517b0ef336785fd0c49f8
Aderson Bertim <adersonbertim@gmail.com>
2022-05-17 23:03:07 -0300

    adding lsp

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -44,8 +44,18 @@ return packer.startup(function(use)
   use "nvim-lua/plenary.nvim" -- Useful lua functions used ny lots of plugins
   --Colorschemes
   use "folke/tokyonight.nvim"
-
-
+    -- cmp plugins
+  use "hrsh7th/nvim-cmp" -- The completion plugin
+  use "hrsh7th/cmp-buffer" -- buffer completions
+  use "hrsh7th/cmp-path" -- path completions
+  use "hrsh7th/cmp-cmdline" -- cmdline completions
+  use "saadparwaiz1/cmp_luasnip" -- snippet completions
+    -- snippets
+  use "L3MON4D3/LuaSnip" --snippet engine
+  use "rafamadriz/friendly-snippets" -- a bunch of snippets to use
+    -- Lsp 
+  use "neovim/nvim-lspconfig" -- enable LSP
+  use "williamboman/nvim-lsp-installer" -- simple to use language server installer
 
 
   if PACKER_BOOTSTRAP then
